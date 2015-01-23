02b9e1428654998f9f96125d5cdd354294e46a38
dramforever <catmimiao@me.com>
2015-01-23 21:54:28 +0800

    Fixed some spacing

--- Interpreter.hs
+++ Interpreter.hs
@@ -74,9 +74,9 @@ defaultMap =
                               ++ "        " ++ show ps
                               ++ "\n    (command was deleted)") : ps ))
              , ("Define", (\ps ->
-                  case ps of
-                   CommandPiece cmd : GroupPiece body : xs ->
-                     xs <$ modify (M.insert cmd $ return . (body ++))
+                 case ps of
+                  CommandPiece cmd : GroupPiece body : xs ->
+                    xs <$ modify (M.insert cmd $ return . (body ++))
                    _ ->
                      return $ 
                      (ErrorPiece $
